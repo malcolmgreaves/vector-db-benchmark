85d3cb12bf0c524b41b5f173340ccde73949e917
Malcolm Greaves <greaves.malcolm@gmail.com>
2023-06-29 15:21:33 -0400

    wip

--- engine/clients/qdrant/configure.py
+++ engine/clients/qdrant/configure.py
@@ -23,7 +23,8 @@ class QdrantConfigurator(BaseConfigurator):
 
     def __init__(self, host, collection_params: dict, connection_params: dict):
         super().__init__(host, collection_params, connection_params)
-
+        import ipdb
+        ipdb.set_trace()
         self.client = QdrantClient(host=host, **connection_params)
 
     def clean(self):
