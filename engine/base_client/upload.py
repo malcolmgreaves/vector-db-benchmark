c9975354405f2929b33a1399365ce296590a2b40
Malcolm Greaves <malcolm.greaves@scale.com>
2023-06-30 22:54:21 -0400

    wip

--- engine/base_client/upload.py
+++ engine/base_client/upload.py
@@ -8,11 +8,11 @@ import tqdm
 from dataset_reader.base_reader import Record
 from engine.base_client.utils import iter_batches
 
-def limit(xs):
-    for i,x in enumerate(xs):
-        if i > 10:
-            break
-        yield x
+# def limit(xs):
+#     for i,x in enumerate(xs):
+#         if i > 10:
+#             break
+#         yield x
         
 class BaseUploader:
     client = None
@@ -67,7 +67,9 @@ class BaseUploader:
                     pool.imap(
                     # pool.map(
                         self._upload_batch,
-                        limit(iter_batches(tqdm.tqdm(records), batch_size)),
+                        # limit(
+                            iter_batches(tqdm.tqdm(records), batch_size)
+                        # ),
                     )
                 )
 
